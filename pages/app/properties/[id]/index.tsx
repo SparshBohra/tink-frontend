f14e8ab3834e20d9268ed6e5107658a97b28d41f
sparshbohra <sparshbohra@gmail.com>
2025-10-16 00:50:52 -0400

    build fix - index.tsx

---
+++ pages/app/properties/[id]/index.tsx
@@ -0,0 +1,16 @@
+import type { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.params || {};
+
+  return {
+    redirect: {
+      destination: `/properties/${id}`,
+      permanent: false,
+    },
+  };
+};
+
+export default function AppPropertyRedirect() {
+  return null;
+}
